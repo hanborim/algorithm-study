7820a8886ad3825f55a3b092b0df81c6090c89e2
hanborim <88933801+hanborim@users.noreply.github.com>
2021-08-16 17:40:00 +0900

    Update borim_3_4.java

--- borim_3_4.java
+++ borim_3_4.java
@@ -1,28 +1,31 @@
 import java.util.*;
 
 public class borim_3_4 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // N, M을 공백을 기준으로 구분하여 입력 받기
+        // N, K를 공백을 기준으로 구분하여 입력 받기
         int n = sc.nextInt();
-        int m = sc.nextInt();
+        int k = sc.nextInt();
         int result = 0;
 
-        // 한 줄씩 입력 받아 확인하기
-        for (int i = 0; i < n; i++) {
-            // 현재 줄에서 '가장 작은 수' 찾기
-            int min_value = 10001;
-            for (int j = 0; j < m; j++) {
-                int x = sc.nextInt();
-                min_value = Math.min(min_value, x);
-            }
-            // '가장 작은 수'들 중에서 가장 큰 수 찾기
-            result = Math.max(result, min_value);
+        while (true) {
+            // N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
+            int target = (n / k) * k;
+            result += (n - target);
+            n = target;
+            // N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+            if (n < k) break;
+            // K로 나누기
+            result += 1;
+            n /= k;
         }
 
-        System.out.println(result); // 최종 답안 출력
+        // 마지막으로 남은 수에 대하여 1씩 빼기
+        result += (n - 1);
+        System.out.println(result);
     }
+
 
 
 }
